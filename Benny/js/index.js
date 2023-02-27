3f3007414d930168441aa722d9df5e5b83e9de14
Bernardo <bernardo@pop-os.localdomain>
2023-02-27 14:19:41 -0300

    trabalho

--- Benny/js/index.js
+++ Benny/js/index.js
@@ -1,8 +1,2 @@
 
-const collapseElementList = document.querySelectorAll('.collapse')
-const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
 
-const bsCollapse = new bootstrap.Collapse('#myCollapse', {
-    toggle: false
-  })
-  
