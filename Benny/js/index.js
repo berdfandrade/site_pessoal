bf6f5fe9e4f77add1e92b640830f7860cf7e691d
Bernardo <bernardo@pop-os.localdomain>
2023-02-27 21:25:26 -0300

    trabalhando

--- Benny/js/index.js
+++ Benny/js/index.js
@@ -1,2 +1,11 @@
 
 
+
+// Aqui criamos uma constaante card. E veja só... Agora faz sentido a parada do javascript de tansformar tudo em variavel. Porque quando você vai manipular a DOM, fica mais simples de entedner que elemento é cada elemento
+
+const card = document.querySelector('.card');
+
+card.addEventListener('click', function() {
+  alert('Você clicou no card!');
+});
+
