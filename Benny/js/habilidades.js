6db5800af03f2e2ac571935c140ec6c42c48d780
Bernardo Win <berdfandrade@gmail.com>
2023-03-17 22:36:28 -0300

    atualizando página de habilidades

--- Benny/js/habilidades.js
+++ Benny/js/habilidades.js
@@ -1,7 +1,24 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "400px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
 const js = document.getElementById("JS");
 
 js.addEventListener("click", function () {
-  window.open("/sub-paginas/javascript.html");
+  openNav();
+  // window.open("/sub-paginas/javascript.html");
+});
+
+js.addEventListener("dblclick", function () {
+  closeNav();
+  // window.open("/sub-paginas/javascript.html");
 });
 
 const html = document.getElementById("HTML");
@@ -30,6 +47,17 @@ python.addEventListener("click", function () {
 
 const c = document.getElementById("C");
 
+
+
 c.addEventListener("click", function () {
-  window.open("/sub-paginas/c.html");
+  // window.open("/sub-paginas/c.html");
+  sidebar.classList.add("sidebar-visivel")
+
 });
+
+const bot = document.getElementById('butola')
+
+bot.addEventListener("dblclick", function(){
+  closeNav();
+})
+
